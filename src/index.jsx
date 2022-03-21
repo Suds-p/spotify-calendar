4d027786ea6956a78fb235eaac4bef54c7acae8a
Suds P <ssss.pontula@gmail.com>
2022-03-21 19:10:43 -0400

    Remove unnecessary bits from index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 
-ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root'));
-
-// Not sure where to put this yet...
-
-// fetch(`http://localhost:5000/tracks/2021-06`)
-// .then(res => res.json())
-// .then(urls => { /* TODO: add code here later */ })
-// .catch(err => {
-//   console.log("* something went very wrong *");
-//   console.log(err);
-// });
+ReactDOM.render(<App />, document.getElementById('root'));
